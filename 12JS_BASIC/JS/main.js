19adfa49099f1cc2a817cef0bb042ada134d09c1
seongjin0902 <ksj05099@naver.com>
2023-07-05 15:47:11 +0900

    0705

---
+++ 12JS_BASIC/JS/main.js
@@ -0,0 +1,8 @@
+console.log("main.js 연결확인!");
+
+
+const boxEl = document.querySelector('.BOX');
+// boxEl.addEventListener('click',function(){});
+boxEl.addEventListener('click',()=>{
+    boxEl.style.backgroundColor="lightgreen";
+})
